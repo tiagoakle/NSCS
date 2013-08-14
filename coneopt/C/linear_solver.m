521200053c0e165843b8a5923fdc3d070fad3f74
tiagoakle <tiagoakle@gmail.com>
2013-08-14 15:29:14 -0700

    added suite sparse and the start of the externally callable linear system solver

---
+++ coneopt/C/linear_solver.m
@@ -0,0 +1,8 @@
+%This function calls the dynamically loadable
+%library to solve a linear system
+
+function x = linear_solver(A,b)
+    %We assume that the library with name liblinear_solvers has been loaded
+
+end
+
---
+++ coneopt/C/linear_solvers.h
@@ -0,0 +1,2 @@
+
+void solve_linear_system(double* x, int* I, int* J, double* V, double *b ,int n);
